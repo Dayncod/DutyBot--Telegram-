b608c012085963a0d990f5e8cef071eb34d31eb9
Dayncod <132489003+Dayncod@users.noreply.github.com>
2024-07-04 17:12:45 +0300

    Updated the algorithm for adding data to the database

--- Main/telebotTest.py
+++ Main/telebotTest.py
@@ -6,11 +6,7 @@ from telebot import types
 
 bot = telebot.TeleBot('6366348527:AAFcFWXB57aK06gZFdRn-Bdc0YNVcijb0C0')
 
-key = (
-      "DRIVER={SQL Server};"
-      "SERVER=DESKTOP-SMAKMB9\DIMONSQLSERVER;"
-      "DATABASE=Customers;"
-      "Trusted_Connection=yes;")
+key = "DRIVER={SQL Server}; SERVER=DESKTOP-SMAKMB9\DIMONSQLSERVER; DATABASE=Customers; Trusted_Connection=yes;"
 
 connect = pyodbc.connect(key)
 cursor = connect.cursor()
@@ -55,36 +51,45 @@ def choose_start(message):
   std = Student(message.chat.id, message.from_user.first_name, 'user')
   Buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
+  cursor.execute(f"SELECT CASE WHEN EXISTS(SELECT 1 FROM Users WHERE id = {std.id}) THEN 1 ELSE 0 END")
+  answer = re.sub("[^0-9]", "", str(cursor.fetchall()))
+
+  if(int(answer) == 0):
+    if(std.id in admins):
+      std.status = 'adm'
+    else:
+      std.status = 'user'
+    cursor.execute(f"INSERT Users(ID, MAIN_NAME, USER_STATUS) VALUES ({std.id}, '{std.name}', '{std.status}')")
+    cursor.commit()
+    print(f"Регистрация Пользователя {std.name}: [{std.id}, {std.status}] прошла успешно")
+  elif(int(answer) == 1):
+    if(std.id in admins):
+      std.status = 'adm'
+      cursor.execute(f"UPDATE Users SET USER_STATUS = 'adm' WHERE ID = {std.id}")
+      cursor.commit()
+    else:
+      std.status = 'user'
+      cursor.execute(f"UPDATE Users SET USER_STATUS = 'user' WHERE ID = {std.id}")
+      cursor.commit()
+    print(f'Пользователь {std.name}: [{std.id}, {std.status}] уже зарегестрирован')
+
   if(std.id in admins):
-    std.status = 'adm'
     btn1 = types.KeyboardButton('Список присутствующих')
     btn2 = types.KeyboardButton('Кто дежурит')
     btn3 = types.KeyboardButton('Изменить список присутсвующих')
     Buttons.add(btn1, btn2, btn3)
   else:
-    cursor.execute(f"SELECT CASE WHEN EXISTS(SELECT 1 FROM Users WHERE id = {std.id}) THEN 1 ELSE 0 END")
-    answer = re.sub("[^0-9]", "", str(cursor.fetchall()))
-
-    if(int(answer) == 0):
-      cursor.execute(f"INSERT Users(ID, MAIN_NAME, USER_STATUS) VALUES ({std.id}, '{std.name}', '{std.status}')")
-      cursor.commit()
-      print(f"Регистрация Пользователя {std.name}: [{std.id}, {std.status}] прошла успешно")
-    elif(int(answer) == 1):
-      print(f'Пользователь {std.name}: [{std.id}, {std.status}] уже зарегестрирован')
-
-    print(answer)
     btn1 = types.KeyboardButton('Список присутствующих')
     btn2 = types.KeyboardButton('Кто дежурит')
     Buttons.add(btn1, btn2)
   bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}, я бот-дежурный для группы З-3-9Б-21, что ты хочешь узнать?', reply_markup=Buttons)
 
 
-
 @bot.message_handler(content_types=['text'])
 def choose_duty(message):
   if(message.text == 'Список присутствующих'):
     for i in range(len(students)):
-      bot.send_message(message.chat.id, f'{i + 1}. {students[i]}')
+      bot.send_message(message.chat.id, f'{i + 1}. {students[i]}\n')
   elif(message.text == 'Кто дежурит'):
     duty_pair = random.sample(students, 2)
     bot.send_message(message.chat.id, f"Дежурят: {duty_pair[0]}, {duty_pair[1]}")
